a5a5cc73de179ebd09f7992adb7de56b924cf7f6
980373688@qq.com <980373688@qq.com>
2021-04-29 21:06:33 +0800

    表格探索

--- src/views/Abc/Components/Table.vue
+++ src/views/Abc/Components/Table.vue
@@ -1,55 +1,90 @@
 <template>
   <div>
-    <div>我是table,我要做点什么</div>
-    <div>
+    <div>我是table,我要做点什么123</div>
       <el-table
           :data="tableData"
           :stripe= "true"
           style="width: 100%">
-        <el-table-column
-            prop="date"
-            label="日期"
-            width="180">
-        </el-table-column>
-        <el-table-column
-            prop="name"
-            label="姓名"
-            width="180">
-        </el-table-column>
-        <el-table-column
-            prop="address"
-            label="地址">
-        </el-table-column>
+        <template v-for="(item, index) in column">
+          <el-table-column
+              v-bind={...item}
+              v-if="!item.v_if"
+              :key="item.prop + index"
+          >
+          </el-table-column>
+        </template>
       </el-table>
-    </div>
+    <ul >
+      <li v-for="(item, index) in column" :key="item.prop">{{item.label + index + JSON.stringify(item)}}
+      <div>{{item.template}}</div>
+        <!--<div><{{item.template}} /></div>-->
+        <div><mycom1/></div></li>
+    </ul>
   </div>
+
 </template>
 
 <script>
-    export default {
-        name: "Table",
-        data() {
-            return {
-                tableData: [{
-                    date: '2016-05-02',
-                    name: '王小虎',
-                    address: '上海市普陀区金沙江路 1518 弄'
-                }, {
-                    date: '2016-05-04',
-                    name: '王小虎',
-                    address: '上海市普陀区金沙江路 1517 弄'
-                }, {
-                    date: '2016-05-01',
-                    name: '王小虎',
-                    address: '上海市普陀区金沙江路 1519 弄'
-                }, {
-                    date: '2016-05-03',
-                    name: '王小虎',
-                    address: '上海市普陀区金沙江路 1516 弄'
-                }]
-            }
-        }
+  import Vue from "vue";
+  Vue.component('mycom1', Vue.extend({
+    template: '<h3>这是使用 Vue.extend 创建的组件</h3>'
+  }))
+  // new Vue({ el: '#app' })
+
+  export default {
+    name: "Table",
+
+
+    data() {
+      return {
+        tableData: [{
+          date: '2016-05-02',
+          name: '王小虎',
+          id: '1',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          id: '2',
+          address: '上海市普陀区金沙江路 1517 弄'
+        }, {
+          date: '2016-05-01',
+          name: '王小虎',
+          id: '3',
+          address: '上海市普陀区金沙江路 1519 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          id: '4',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }],
+        column: [
+          {
+            prop: 'date',
+            label: '日期',
+            width: '180',
+          },
+          {
+            prop: 'name',
+            label: '姓名',
+            width: '180',
+          },
+          {
+            prop: 'address',
+            label: '地址',
+          },
+          {
+            prop: 'id',
+            label: 'id地址',
+            width: '180',
+            v_if: true,
+            template: 'mycom1'
+          },
+        ],
+        isIdDis: false,
+      }
     }
+  }
 </script>
 
 <style scoped>
